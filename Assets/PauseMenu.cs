85b8386d79cdb6f571547c691c346abdf6965d73
cadaralex12 <alexcadar2000@gmail.com>
2023-04-03 23:33:59 +0300

    added pause screen for Endless and Tutorial

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -29,17 +30,22 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void Resume()
+    public void Resume()
     {
         PauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
-    void Pause()
+    public void Pause()
     {
         PauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("LevelSelection");
+        Time.timeScale = 1f;
     }
 }
