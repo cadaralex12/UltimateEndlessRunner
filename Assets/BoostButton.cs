bc84950785a407bc4d8c07fbc10aa62c064dc6fa
cadaralex12 <alexcadar2000@gmail.com>
2023-04-19 19:30:24 +0300

    powerups progress bars + minor UI tweaks

--- Assets/BoostButton.cs
+++ Assets/BoostButton.cs
@@ -1,14 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoostButton : MonoBehaviour
 {
-    public Movement player;
+public Movement player;
+public Image timerBar;
+public float duration = 3.0f;
+private bool boostActive = false;
+private float timeRemaining = 0.0f;
+private float barWidth = 200.0f;
+private Color barColor;
 
-    public void ActivateBoost()
+private void Awake()
+{
+    barWidth = timerBar.rectTransform.sizeDelta.x;
+    barColor = timerBar.color;
+}
+
+private void Update()
+{
+    if (boostActive)
     {
-         player.boostPressed = true;
-         player.Boost();
+        timeRemaining -= Time.deltaTime;
+
+        if (timeRemaining <= 0.0f)
+        {
+            boostActive = false;
+            player.boostPressed = false;
+            timerBar.gameObject.SetActive(false);
+        }
+        else
+        {
+            float t = timeRemaining / duration;
+            float progress = 1.0f - t;
+            float barFill = progress * barWidth;
+            timerBar.rectTransform.sizeDelta = new Vector2(barWidth - barFill, timerBar.rectTransform.sizeDelta.y);
+
+            float r = 120;
+            float g = barColor.g;
+            float b = 0;
+            timerBar.color = new Color(r, g, b);
+        }
     }
+}
+
+public void ActivateBoost()
+{
+    if (!boostActive)
+    {
+        boostActive = true;
+        player.boostPressed = true;
+        player.Boost();
+
+        timeRemaining = duration;
+        timerBar.gameObject.SetActive(true);
+        timerBar.rectTransform.sizeDelta = new Vector2(barWidth, timerBar.rectTransform.sizeDelta.y);
+        timerBar.color = barColor;
+    }
+}
 }
--- Assets/SlowMotionButton.cs
+++ Assets/SlowMotionButton.cs
@@ -1,14 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlowMotionButton : MonoBehaviour
 {
     public Movement player;
+    public Image timerBar;
+    public float duration = 5.0f;
+
+    private bool slowMotionActive = false;
+    private float timeRemaining = 0.0f;
+    private float barWidth = 200.0f;
+    private Color barColor;
+
+    private void Awake()
+    {
+        barWidth = timerBar.rectTransform.sizeDelta.x;
+        barColor = timerBar.color;
+    }
+
+    private void Update()
+    {
+        if (slowMotionActive)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            if (timeRemaining <= 0.0f)
+            {
+                slowMotionActive = false;
+                player.slowMoPressed = false;
+                timerBar.gameObject.SetActive(false);
+            }
+            else
+            {
+                float t = timeRemaining / duration;
+                float progress = 1f - t;
+                float barFill = progress * barWidth;
+                timerBar.rectTransform.sizeDelta = new Vector2(barWidth - barFill, timerBar.rectTransform.sizeDelta.y);
+
+                float r = 120;
+                float g = barColor.g * t;
+                float b = 0;
+                timerBar.color = new Color(r, g, b);
+            }
+        }
+    }
 
     public void ActivateSlowMotion()
     {
-        player.slowMoPressed = true;
-        player.BulletTime();
+        if (!slowMotionActive)
+        {
+            slowMotionActive = true;
+            player.slowMoPressed = true;
+            player.BulletTime();
+
+            timeRemaining = duration;
+            timerBar.gameObject.SetActive(true);
+            timerBar.rectTransform.sizeDelta = new Vector2(barWidth, timerBar.rectTransform.sizeDelta.y);
+            timerBar.color = barColor;
+        }
     }
 }
